33e6a87747375cc142458b5d4aad4dac9148befe
DanGonite57 <danzinc57@gmail.com>
2023-11-04 15:01:39 +0000

    Try versioneer 0.19

--- pprofile/__init__.py
+++ pprofile/__init__.py
@@ -1542,3 +1542,7 @@ del pprofile
 
 from . import _version
 __version__ = _version.get_versions()['version']
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
